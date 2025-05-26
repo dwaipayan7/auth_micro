4bce59d94c8ace43e61fe066df256c5190f6e4c7
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-05-26 14:59:34 +0530

    updated

---
+++ controller/user.controller.js
@@ -0,0 +1,27 @@
+import prisma from "../config/db.js";
+import { config } from "dotenv";
+config();
+
+class UserController {
+  static async getUser(req, res) {
+    try {
+      const  id  = req.params.id;
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: id,
+        },
+      });
+
+      return res.status(200).json({
+        message: "User found",
+        user: user,
+      });
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+}
+
+export default UserController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import AuthRoutes from "./auth.routes.js";
-
+import UserRoutes from "./user.routes.js";
 const router = Router();
 
 router.use("/api", AuthRoutes);
+router.use("/api", UserRoutes);
 
 
 export default router;
